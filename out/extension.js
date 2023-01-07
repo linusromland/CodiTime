dfef8956fcc67b10eb888a6fc0f74b2dac9b4060
Linus Romland <hello@linusromland.com>
2023-01-07 18:03:04 +0100

    Added basic time tracking logging

--- out/extension.js
+++ out/extension.js
@@ -4,14 +4,43 @@ exports.deactivate = exports.activate = void 0;
 // External dependencies
 const vscode_1 = require("vscode");
 function activate(context) {
-    console.log('CodeTracker successfully activated!');
-    //Hello World command
-    const disposable = vscode_1.commands.registerCommand('codetracker.helloWorld', () => {
-        vscode_1.window.showInformationMessage('Hello World from CodeTracker!');
+    const disposable = vscode_1.commands.registerCommand('codetracker.activate', () => {
+        vscode_1.window.showInformationMessage('CodeTracker activated!');
+        startCodeTracker();
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+function startCodeTracker() {
+    // Get OS and hostname
+    const operatingSystem = process.platform;
+    const hostname = require('os').hostname();
+    // Initialize the startTime, previousDocument and workspaceName variables
+    let startTime = new Date();
+    let previousDocument = vscode_1.window.activeTextEditor?.document.uri.path;
+    let workspaceName = vscode_1.workspace.workspaceFolders?.[0].name;
+    vscode_1.window.onDidChangeActiveTextEditor(() => {
+        const activeDocument = vscode_1.window.activeTextEditor?.document.uri.path;
+        // If there is no active document, return
+        if (!activeDocument) {
+            return;
+        }
+        // Calculate the time spent on the previous document
+        const timeSpent = new Date().getTime() - startTime.getTime();
+        // Log the data
+        console.log('\n----------------------------------------------------------------');
+        console.log(`Time spent: ${timeSpent}ms`);
+        console.log(`File: ${previousDocument}`);
+        console.log(`Workspace: ${workspaceName}`);
+        console.log(`OS: ${operatingSystem}`);
+        console.log(`Hostname: ${hostname}`);
+        console.log('----------------------------------------------------------------\n');
+        // Update the startTime, previousDocument and workspaceName variables
+        startTime = new Date();
+        previousDocument = activeDocument;
+        workspaceName = vscode_1.workspace.workspaceFolders?.[0].name;
+    });
+}
 function deactivate() {
     console.log('CodeTracker successfully deactivated!');
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,50 @@
 // External dependencies
-import { ExtensionContext, commands, window } from 'vscode';
+import { ExtensionContext, commands, window, workspace } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-	console.log('CodeTracker successfully activated!');
-
-	//Hello World command
-	const disposable = commands.registerCommand('codetracker.helloWorld', () => {
-		window.showInformationMessage('Hello World from CodeTracker!');
+	const disposable = commands.registerCommand('codetracker.activate', () => {
+		window.showInformationMessage('CodeTracker activated!');
+		startCodeTracker();
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function startCodeTracker() {
+	// Get OS and hostname
+	const operatingSystem = process.platform;
+	const hostname = require('os').hostname();
+
+	// Initialize the startTime, previousDocument and workspaceName variables
+	let startTime = new Date();
+	let previousDocument = window.activeTextEditor?.document.uri.path;
+	let workspaceName = workspace.workspaceFolders?.[0].name;
+
+	window.onDidChangeActiveTextEditor(() => {
+		const activeDocument = window.activeTextEditor?.document.uri.path;
+
+		// If there is no active document, return
+		if (!activeDocument) {
+			return;
+		}
+
+		// Calculate the time spent on the previous document
+		const timeSpent = new Date().getTime() - startTime.getTime();
+
+		// Log the data
+		console.log('\n----------------------------------------------------------------');
+		console.log(`Time spent: ${timeSpent}ms`);
+		console.log(`File: ${previousDocument}`);
+		console.log(`Workspace: ${workspaceName}`);
+		console.log(`OS: ${operatingSystem}`);
+		console.log(`Hostname: ${hostname}`);
+		console.log('----------------------------------------------------------------\n');
+
+		// Update the startTime, previousDocument and workspaceName variables
+		startTime = new Date();
+		previousDocument = activeDocument;
+		workspaceName = workspace.workspaceFolders?.[0].name;
+	});
 }
 
 export function deactivate() {
