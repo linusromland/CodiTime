2e6537d753b3db6775e0d3906042557fa2ff4b00
Linus Romland <hello@linusromland.com>
2023-01-11 19:04:19 +0100

    Added logs for when heartbeats are created

--- src/routes/heartbeats/heartbeats.controller.ts
+++ src/routes/heartbeats/heartbeats.controller.ts
@@ -1,5 +1,5 @@
 // External dependencies
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, Post, Logger } from '@nestjs/common';
 import { Heartbeat, Response } from 'src/interfaces';
 
 // Internal dependencies
@@ -7,13 +7,20 @@ import { HeartbeatsService } from './heartbeats.service';
 
 @Controller('heartbeats')
 export class HeartbeatsController {
+	private readonly logger = new Logger(HeartbeatsController.name);
+
 	constructor(private readonly heartbeatsService: HeartbeatsService) {}
 
 	@Post()
 	async create(@Body() body: { project: string; heartbeat: Heartbeat }) {
 		const response: Response = await this.heartbeatsService.create(body.project, body.heartbeat);
 
-		if (!response || !response.success) throw new HttpException(response.message, 400);
+		if (!response || !response.success) {
+			this.logger.log('Heartbeat creation failed');
+			throw new HttpException(response.message, 400);
+		}
+
+		this.logger.log('Heartbeat created successfully');
 
 		return response;
 	}
